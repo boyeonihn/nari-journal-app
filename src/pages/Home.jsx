8d15755c53097c1c53f573bdeb927cc2b1bfc8f0
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-09 15:20:16 +0900

    feat: implement header and button component in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import Header from '../components/Header';
+import Button from '../components/Button';
+
 const Home = () => {
   return (
     <section>
       <h1>Home</h1>
+      <Header
+        headText="Nari Journal"
+        leftChild={
+          <Button text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />
+        }
+        rightChild={
+          <Button text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭')} />
+        }
+      />
       <p>이곳은 Home입니다</p>
     </section>
   );
