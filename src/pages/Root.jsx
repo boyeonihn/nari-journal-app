4d433da8729a9cf70bdf6fd497e587fc58710894
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-09 15:18:36 +0900

    feat: add reducer function to manipulate diaryData

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,33 @@
 import { Outlet } from 'react-router-dom';
 import RouteTest from '../components/RouteTest';
+import React, { useReducer, useRef } from 'react';
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      newState = [action.data, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((n) => n.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((n) =>
+        n.id === action.data.id ? { ...action.data } : n
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
 
 export default function Root() {
   return (
