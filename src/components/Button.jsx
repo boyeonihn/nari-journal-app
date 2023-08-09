37918f4d175325eda1bf4b1b3f6d9ed3ee8f7cd1
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-09 15:17:09 +0900

    feat: create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+const Button = ({ text, type, onClick }) => {
+  const btnType = ['positive', 'negative'].includes(type) ? type : 'default';
+  return (
+    <button
+      onClick={onClick}
+      className={['Button', `Button_${btnType}`].join(' ')}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'default',
+};
+export default Button;
