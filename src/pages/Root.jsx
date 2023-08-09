a5d697e6f2be247e56f74d376c2ab4b770fd3cdd
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-09 15:19:06 +0900

    feat: create dataState and handlers for data manipulation

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -30,6 +30,43 @@ const reducer = (state, action) => {
 
 
 export default function Root() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: 'REMOVE',
+      targetId,
+    });
+  };
+
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
     <>
       <h1>나리 기록들</h1>
