6948893570319f643d874b6ec6ea7bcb4decf356
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-14 19:44:43 +0900

    feat: create functions to increase/decrease Month of calendar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,25 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 
 const Home = () => {
+  const increaseMonth = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        currentDate.getDate()
+      )
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() - 1,
+        currentDate.getDate()
+      )
+    );
+  };
   return (
     <section>
       <Header
