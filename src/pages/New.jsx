5b6ab752f790fd58dcc0668934e5d969720056c4
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-14 19:42:18 +0900

    feat: incorporate Header component in New component

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,6 +1,8 @@
+import Header from '../components/Header';
 const New = () => {
   return (
     <section>
+      <Header headText="New Entry" />
       <h1>New</h1>
       <p>이곳은 New입니다</p>
     </section>
