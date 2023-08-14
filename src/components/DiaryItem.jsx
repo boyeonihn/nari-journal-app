fed527e725a3a43fe0eeaaec213c99774c182c22
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-14 19:40:18 +0900

    feat: create DiaryItem Component

---
+++ src/components/DiaryItem.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+export const DiaryItem = ({ text, emotion, createdAt, id }) => {
+  const navigate = useNavigate();
+  const date = new Date(createdAt);
+
+  const goDiaryEntry = () => {
+    navigate(`/diary/${id}`);
+  };
+  return (
+    <section className="DiaryItem">
+      <div
+        onClick={goDiaryEntry}
+        className={['emotionContainer', `emotionContainer_${emotion}`].join(
+          ' '
+        )}
+      >
+        <img src={`/assets/emotion${emotion}.png`} />
+      </div>
+      <div onClick={goDiaryEntry} className="DiaryContent">
+        <h2>{date.toLocaleString('sv').slice(0, 16)}</h2>
+        <p>{text}</p>
+      </div>
+      <Button
+        text="수정하기"
+        onClick={() => {
+          navigate(`/edit/${id}`);
+        }}
+      />
+    </section>
+  );
+};
