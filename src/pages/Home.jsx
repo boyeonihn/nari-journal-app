22da314c9827bcf44ce85fca89c793dd0b0419e2
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-14 19:44:17 +0900

    feat: edit rendered JSX of Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,12 @@ import Button from '../components/Button';
 const Home = () => {
   return (
     <section>
-      <h1>Home</h1>
       <Header
-        headText="Nari Journal"
-        leftChild={
-          <Button text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />
-        }
-        rightChild={
-          <Button text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭')} />
-        }
+        headText={headText}
+        leftChild={<Button text={'<'} onClick={decreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={increaseMonth} />}
       />
-      <p>이곳은 Home입니다</p>
+      <DiaryList diaryList={data} />
     </section>
   );
 };
