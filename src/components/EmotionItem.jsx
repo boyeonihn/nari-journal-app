6c541596604dc49c44ae53666f2bb4c764f540a5
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-22 17:12:17 +0900

    feat: create EmotionItem Component

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,24 @@
+const EmotionItem = ({
+  emotion_img,
+  emotion_descript,
+  emotion_id,
+  onClick,
+  isSelected,
+}) => {
+  return (
+    <div
+      className={[
+        'input_emotion',
+        'EmotionItem',
+        `emotion_img_${emotion_id}`,
+        isSelected ? `EmotionItem_on_${emotion_id}` : '',
+      ].join(' ')}
+      onClick={() => onClick(emotion_id)}
+    >
+      <img src={emotion_img} className="input_emotion_img" />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+};
+
+export default EmotionItem;
