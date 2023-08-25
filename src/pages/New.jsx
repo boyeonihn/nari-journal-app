1b7cdf70e2226f6a8ecef296c1e364ccb4cacc70
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-25 09:54:18 +0900

    feat: edit title of rendered page and site icon

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import DiaryWriter from '../components/DiaryWriter';
 
 const New = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `나리 일기 - 새 일기`;
+  }, []);
+
   return (
     <div>
       <DiaryWriter />
