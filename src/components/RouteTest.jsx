15899d011de194890010ee852628ba11cec4633f
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-25 09:18:34 +0900

    refactor: edit fragment element to <nav> tag

--- src/components/RouteTest.jsx
+++ src/components/RouteTest.jsx
@@ -2,15 +2,14 @@ import { Link } from 'react-router-dom';
 
 const RouteTest = () => {
   return (
-    <>
-      <Link to={'/home'}>HOME</Link>
+    <nav>
+      <Link to={'/'}>HOME</Link>
       <br />
       <Link to={'/diary'}>Diary</Link>
       <br />
       <Link to={'/new'}>New</Link>
       <br />
-      <Link to={'/edit'}>Edit</Link>
-    </>
+    </nav>
   );
 };
 
