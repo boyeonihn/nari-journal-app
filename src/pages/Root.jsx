2a3e5c6e7f035f8d0966caa9449f07d75db8c128
Boyeon Ihn <boyeonihn@gmail.com>
2023-08-30 15:25:29 +0900

    feat: remove useState which is replaced by reducer

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Link } from 'react-router-dom';
-import React, { useReducer, useRef, useEffect, useState } from 'react';
+import React, { useReducer, useRef, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
